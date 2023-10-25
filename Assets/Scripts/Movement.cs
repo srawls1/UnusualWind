6db6b2c357631dfe1979de0eeba1f97124579fb0
Coleman Andersen <cjandersen000021@gmail.com>
2023-10-25 14:02:53 -0700

    Almost all prefabs made
    With colliders. Next I'm adding lightning to storm clouds and populating the level

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,8 @@ public class Movement : MonoBehaviour
 {
     public int movementSpeed; 
     public bool isWindmill; //If true, the object will rotate instead of move
+    public bool isFish; //If true, object will move in an arc
+    public float arcHeight; //How high the arc is
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,13 @@ public class Movement : MonoBehaviour
         {
             //Rotation
             transform.Rotate(0f, 0f, movementSpeed * Time.deltaTime);
+        }
+        else if (isFish)
+        {
+            //Movement
+            transform.position += new Vector3(movementSpeed * Time.deltaTime, Mathf.Sin(Time.time) * arcHeight, 0f);
+            //Rotation
+            transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Sin(Time.time) * arcHeight * 300);
         }
         else
         {
