972507e9dda75ba2d0678785b7d7221ccb174171
Coleman Andersen <cjandersen000021@gmail.com>
2023-10-28 14:04:20 -0700

    Updating the gamemanager script to track time

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private Transform timeOfDayTracker;
+    private Transform playerTransform;
     private Transform sunset;
     private Transform moon;
     private Transform sunrise;
@@ -12,7 +12,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timeOfDayTracker = GameObject.Find("TimeOfDayTracker").GetComponent<Transform>();
+        //find the player tag
+        playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
         sunset = GameObject.Find("Sunset").GetComponent<Transform>();
         moon = GameObject.Find("Moon").GetComponent<Transform>();
         sunrise = GameObject.Find("Sunrise").GetComponent<Transform>();
@@ -26,21 +27,21 @@ public class GameManager : MonoBehaviour
 
     private void SkyUpdate()
     {
-        sunset.position -= new Vector3(0, timeOfDayTracker.position.x, 0);
+        sunset.position -= new Vector3(0, playerTransform.position.x, 0);
 
-        if (timeOfDayTracker.position.x > 1000f && timeOfDayTracker.position.x < 2000f)
+        if (playerTransform.position.x > 1000f && playerTransform.position.x < 2000f)
         {
-            moon.position += new Vector3(0, timeOfDayTracker.position.x, 0);
+            moon.position += new Vector3(0, playerTransform.position.x, 0);
         }
 
-        if (timeOfDayTracker.position.x > 2000f && timeOfDayTracker.position.x < 3000f)
+        if (playerTransform.position.x > 2000f && playerTransform.position.x < 3000f)
         {
-            moon.position -= new Vector3(0, timeOfDayTracker.position.x, 0);
+            moon.position -= new Vector3(0, playerTransform.position.x, 0);
         }
 
-        if (timeOfDayTracker.position.x > 3000f)
+        if (playerTransform.position.x > 3000f)
         {
-            sunrise.position += new Vector3(0, timeOfDayTracker.position.x, 0);
+            sunrise.position += new Vector3(0, playerTransform.position.x, 0);
         }
     }
 }
