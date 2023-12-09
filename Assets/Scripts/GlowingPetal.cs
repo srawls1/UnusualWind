4696245a5a457c086bc9116b481408805e3fbce6
Coleman Andersen <cjandersen000021@gmail.com>
2023-12-08 21:51:03 -0800

    glow petal animation done

--- Assets/Scripts/GlowingPetal.cs
+++ Assets/Scripts/GlowingPetal.cs
@@ -4,23 +4,59 @@ using UnityEngine;
 
 public class GlowingPetal : MonoBehaviour
 {
-    private GameObject gameManager; 
+    [SerializeField] private GameObject gameManager;
+    [SerializeField] private GameObject player;
+    private Animator animator;
+    private float collectSpeed = 15f;
+    private float moveAwaySpeed = 10f;
+    private bool moveTowardsPlayer = false;
+    private bool moveAway = false;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        //Find the game manager using the name
-        gameManager = GameObject.Find("GameManager");
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (moveTowardsPlayer)
+        {
+            //Move towards the player
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, collectSpeed * Time.deltaTime);
+        }
+
+        if (moveAway && !moveTowardsPlayer)
+        {
+            //Move away from the player
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, -moveAwaySpeed * Time.deltaTime);
+        }
     }
 
     private void OnTriggerEnter2D (Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            //Play the collect animation before destroying the object
-            Destroy(gameObject);
+            //Trigger the animation
+            animator.SetTrigger("Collect");
             //Increase petal count from the game manager
             gameManager.GetComponent<GameManager>().petalCount++;
         }
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        moveAway = false;
+        moveTowardsPlayer = true;
+    }
+
+    private void MoveAway()
+    {
+        moveAway = true;
+    }
+
+    private void DestroyPetal()
+    {
+        //Destroy the petal
+        Destroy(gameObject);
     }
 }
